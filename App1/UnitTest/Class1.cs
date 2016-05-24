9b0a33608990d401da70c703596534cf14b5e2f0
benrnz <ben@rees.biz>
2016-05-24 22:08:15 +1200

    Initial commit. Standard Xamarin Forms new App template with NUnit 2.6.4 project.

---
+++ App1/UnitTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
